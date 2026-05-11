b32dd1a2d24833134e934d4f7c7e1697630e51c8
BobDBuilda <nikolaiholder1@gmail.com>
2026-05-10 22:07:21 -0400

    gjjd

--- src/Utils/EstablishConn.js
+++ src/Utils/EstablishConn.js
@@ -1,6 +1,7 @@
 const establishConnection = async() => {
     try{
-        const res = await fetch("http://localhost:3000");
+        // Changed from http://localhost:3000 to relative /api path for Cloudflare Pages/Functions
+        const res = await fetch("/api");
 
         if(!res.ok){
             throw new Error("server no good");
@@ -9,7 +10,7 @@ const establishConnection = async() => {
         const data = await res.json();
         console.log("conn established");
     }catch(error){
-        console.error("failed");
+        console.error("failed", error);
     }
 }
 
